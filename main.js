a6879a607f66c8ac69f67535686034ff85c7f562
Murad Qocayev <muradqocayevi20@gmail.com>
2023-01-27 12:29:27 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// TASK 1
+// let arr = prompt("ededleri daxil edin").split(",");
+
+// let total = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   total += +arr[i];
+// }
+
+// document.getElementById("root").innerHTML = total;
+
+
+// TASK 2
+
+// let arr = prompt('Ədədləri daxil edin').split(',');
+// let total=0;
+
+
+// for(let i = 0; i<arr.length; i++){
+
+//     arr[i] > 0 ? total += Number(arr[i]): true;
+
+
+// }
+
+// document.getElementById("root").innerHTML = total;
+
+
+// TASK 3
+
+// let num = +prompt('Ədəd daxil edin')
+
+// value = 'sifir'
+
+// if(num>0){
+//     value = 'musbet'
+// }else if(num<0){
+//     value = 'menfi'
+// }
+
+// document.getElementById('root').innerHTML= value;
+
+
+// TASK 4
+
+// let num1 = +prompt('Enter number');
+// let num2 = +prompt('Enter number');
+
+// let value=num2;
+// if(num1>num2){
+//     value = num1
+// }
+
+// document.getElementById('root').innerHTML = value;
